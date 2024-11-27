99d7069b5374f34620a7a980922e5cc6ed4f2023
Kate <kzimmermann@kiwi.de>
2024-11-27 14:52:33 +0100

    add basic files

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kiwi\Contao\ResponsiveBase\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
+use Kiwi\Contao\Blueprints\KiwiBlueprints;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\Loader\LoaderResolverInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class Plugin implements BundlePluginInterface, RoutingPluginInterface, ConfigPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser): array
+    {
+        return [
+            BundleConfig::create(KiwiBlueprints::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class
+                ]),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig): void
+    {
+        $loader->load(__DIR__ . '/../../config/services.yaml');
+    }
+}
