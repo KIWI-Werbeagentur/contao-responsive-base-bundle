f4bcc742b0ad5205c675f43bb04748a40b953649
Kate <kzimmermann@kiwi.de>
2024-12-10 19:08:59 +0100

    extend abstract bundle in bundle class

--- src/KiwiResponsiveBaseBundle.php
+++ src/KiwiResponsiveBaseBundle.php
@@ -2,12 +2,8 @@
 
 namespace Kiwi\Contao\ResponsiveBaseBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class KiwiResponsiveBaseBundle extends Bundle
+class KiwiResponsiveBaseBundle extends AbstractBundle
 {
-    public function getPath(): string
-    {
-        return \dirname(__DIR__);
-    }
 }
