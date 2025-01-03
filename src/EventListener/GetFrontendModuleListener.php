d15a42c17d9590647a6cb26c38a8fbc054ea6eea
Kate <kzimmermann@kiwi.de>
2025-01-03 10:05:05 +0100

    Add classes to module

---
+++ src/EventListener/GetFrontendModuleListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kiwi\Contao\ResponsiveBaseBundle\EventListener;
+
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\Hybrid;
+use Contao\ModuleModel;
+use Contao\System;
+use Kiwi\Contao\CmxBundle\DataContainer\PaletteManipulatorExtended;
+
+#[AsHook('getFrontendModule')]
+class GetFrontendModuleListener
+{
+    public function __invoke(ModuleModel $objModuleModel, string $strBuffer, object $objModule): string
+    {
+        $objFrontendModule = $objModule instanceof Hybrid ? $objModule->getParent() : $objModuleModel;
+
+        $isField = PaletteManipulatorExtended::create()->hasField($objFrontendModule->type, 'tl_module', 'addResponsive');
+
+        if ($objFrontendModule->addResponsive && $isField) {
+            $arrClasses = System::getContainer()->get('kiwi.contao.responsive.frontend')->getAllResponsiveClasses($objFrontendModule->row());
+
+            $strBootstrapClasses = implode(' ', $arrClasses);
+
+            if ($strBootstrapClasses) {
+                if ($objModule->Template) {
+                    $objModule->Template->class = trim($objModule->Template->class . ' ' . $strBootstrapClasses);
+                    $strBuffer = $objModule->Template->parse();
+                }
+            }
+        }
+
+        return $strBuffer;
+    }
+}
