75917050653696cbbc72c3aa01ec74c10ca9bd34
Kate <kzimmermann@kiwi.de>
2025-01-08 11:03:28 +0100

    Add layout classes

--- src/EventListener/ParseTemplateListener.php
+++ src/EventListener/ParseTemplateListener.php
@@ -15,135 +15,84 @@ class ParseTemplateListener
     public function __invoke($objTemplate)
     {
         if ($objTemplate->typePrefix == 'mod_') {
-            Controller::loadDataContainer('responsive');
-
-            if (!$objTemplate->addResponsiveChildren || !$objTemplate->responsiveColsItems) return;
-
-            //Checks if DCA-Palette has Settings for children (usually used for lists)
-            if (!in_array($objTemplate->type, array_keys($GLOBALS['responsive']['tl_module']['includePalettes']['container']))) return;
-
-            $strColumnClasses = implode(" ", System::getContainer()->get('kiwi.contao.responsive.frontend')->getColClasses($objTemplate->responsiveColsItems));
-            $varChildren = ($objTemplate->{$GLOBALS['responsive']['tl_module']['includePalettes']['container'][$objTemplate->type]});
-
-            if (is_array($varChildren) && $objTemplate->isResponsive) {
-                foreach ($varChildren as &$varChild) {
-                    $varChild = System::getContainer()->get('twig')->render('@KiwiResponsiveBase/list_child.html.twig', [
-                        'baseClass' => $objTemplate->baseClass,
-                        'class' => $strColumnClasses,
-                        'item' => $varChild
-                    ]);
-                }
-            }
-
-            $objTemplate->{$GLOBALS['responsive']['tl_module']['includePalettes']['container'][$objTemplate->type]} = $varChildren;
+            self::wrapListItems($objTemplate);
         }
-        return;
-        if ($objTemplate->typePrefix == 'mod_') {}
-        /**
-         * fe_page is still a legacy template
-         */
-        elseif (substr($objTemplate->getName(), 0, 7) == 'fe_page') {
-            $bootstrapClasses = [];
 
-            $sidebars = [];
-            if ($objTemplate->layout->cols == '3cl') {
-                $sidebars[] = "left";
-                $sidebars[] = "right";
-            } elseif ($objTemplate->layout->cols == '2cll') {
-                $sidebars[] = "left";
-            } elseif ($objTemplate->layout->cols == '2clr') {
-                $sidebars[] = "right";
+        if(substr($objTemplate->getName(), 0, 7) == 'fe_page'){
+            self::setLayoutSizes($objTemplate);
+        }
+    }
+
+    public static function wrapListItems(&$objTemplate){
+        Controller::loadDataContainer('responsive');
+
+        if (!$objTemplate->addResponsiveChildren || !$objTemplate->responsiveColsItems) return;
+
+        //Checks if DCA-Palette has Settings for children (usually used for lists)
+        if (!in_array($objTemplate->type, array_keys($GLOBALS['responsive']['tl_module']['includePalettes']['container']))) return;
+
+        $strColumnClasses = implode(" ", System::getContainer()->get('kiwi.contao.responsive.frontend')->getColClasses($objTemplate->responsiveColsItems));
+        $varChildren = ($objTemplate->{$GLOBALS['responsive']['tl_module']['includePalettes']['container'][$objTemplate->type]});
+
+        if (is_array($varChildren) && $objTemplate->isResponsive) {
+            foreach ($varChildren as &$varChild) {
+                $varChild = System::getContainer()->get('twig')->render('@KiwiResponsiveBase/list_child.html.twig', [
+                    'baseClass' => $objTemplate->baseClass,
+                    'class' => $strColumnClasses,
+                    'item' => $varChild
+                ]);
             }
+        }
 
-            $total = [];
+        $objTemplate->{$GLOBALS['responsive']['tl_module']['includePalettes']['container'][$objTemplate->type]} = $varChildren;
+    }
 
-            //Alle Breakpoints holen und nach Größe sortieren
-            $arrBreakpoints = BootstrapListener::getBreakpoints();
+    public static function mapSidebars(&$objTemplate){
+        $sidebars = [];
+        if ($objTemplate->layout->cols == '3cl') {
+            $sidebars[] = "Left";
+            $sidebars[] = "Right";
+        } elseif ($objTemplate->layout->cols == '2cll') {
+            $sidebars[] = "Left";
+        } elseif ($objTemplate->layout->cols == '2clr') {
+            $sidebars[] = "Right";
+        }
+        return $sidebars;
+    }
 
-            uasort($arrBreakpoints, function ($a, $b) {
-                if ($a['size'] == $b['size']) {
-                    return 0;
-                }
-                return ($a['size'] < $b['size']) ? -1 : 1;
-            });
+    public static function setLayoutSizes(&$objTemplate){
+        $responsiveFrontendService = System::getContainer()->get('kiwi.contao.responsive.frontend');
 
-            // Standardwert für die Seitenspaltenbreite beim kleinsten Breakpoint.
-            // Damit #main .col-Klassen bekommt, auch wenn es keine Seitenspalten gibt.
-            $total[array_keys($arrBreakpoints)[0]] = 0;
+        // Columns
+        $arrBreakpoints = [];
+        $objTemplate->responsiveColsLeft = [];
 
-            foreach ($sidebars as $sidebar) {
-                $arrBreakpointSetting = StringUtil::deserialize($objTemplate->layout->{'col_' . $sidebar});
+        foreach (self::mapSidebars($objTemplate) as $sidebar) {
+            $arrBreakpointSetting = StringUtil::deserialize($objTemplate->layout->{'responsiveCols' . $sidebar});
+            $objTemplate->{'responsiveCols' . $sidebar} = $responsiveFrontendService->getColClasses($objTemplate->layout->{'responsiveCols' . $sidebar});
 
-                $lastCol = 0;
-                foreach ($arrBreakpoints as $arrBreakpoint) {
-                    $col = $arrBreakpointSetting[$arrBreakpoint['name']];
-                    if ($col == 'inherit') {
-                        $col = $lastCol;
-                    }
-
-                    if (isset($total[$arrBreakpoint['name']])) {
-                        $total[$arrBreakpoint['name']] += (int)$col;
-                    } else {
-                        $total[$arrBreakpoint['name']] = (int)$col;
-                    }
-
-                    if ($lastCol != $col || $col == 'none-only') {
-                        if ($col == 'none-only') {
-                            $base = 'd-';
-                        } else {
-                            $base = 'col-';
-                        }
-                        if ($arrBreakpoint['name'] == 'xs') {
-                            $breakpoint = '';
-                        } else {
-                            $breakpoint = $arrBreakpoint['name'] . '-';
-                        }
-                        $bootstrapClasses[$sidebar][$arrBreakpoint['name']] = $base . $breakpoint . $col;
-                    }
-                    $lastCol = $col;
-                }
+            $prevVal = 0;
+            foreach((new $GLOBALS['responsive']['config']())->arrBreakpoints as $strBreakpoint => $arrBreakpoint){
+                if($arrBreakpointSetting[$strBreakpoint] ?? false) $prevVal = $arrBreakpointSetting[$strBreakpoint];
+                $arrBreakpoints[$strBreakpoint] = ($arrBreakpoints[$strBreakpoint] ?? 0) + ($arrBreakpointSetting[$strBreakpoint] ?? $prevVal);
             }
-            $bootstrapClasses["left"][] = "order-first";
+        }
 
-            // Jeweils mit dem vorherigen Wert vergleichen, damit Klassen, die sich vererben würden, nicht explizit gesetzt werden.
-            $lastSize = null;
-            foreach ($total as $breakpoint => $col) {
-                $size = 12 - ($col % 12);
-                if ($lastSize !== $size) {
-                    if ($breakpoint == 'xs') {
-                        $breakpoint = '';
-                    } else {
-                        $breakpoint .= '-';
-                    }
-                    $bootstrapClasses['main'][$breakpoint] = "col-" . $breakpoint . $size;
-                }
-                $lastSize = $size;
-            }
+        $objTemplate->responsiveColsLeft[] = "order-first";
 
-            foreach ($bootstrapClasses as $column => $classes) {
-                $objTemplate->{$column . 'Class'} = implode(' ', $classes);
-            }
 
-            // Container-Klassen für #header, #container und #footer setzen
-            $arrSections = ['header', 'container', 'footer'];
-            foreach ($arrSections as $section) {
-                // $property = $section.'InsideClass';
-                // if ($objTemplate->layout->{'bootstrap_'.$section.'_width'} == 'container-fluid') {
-                //     $property = $section.'Class';
-                // }
-                $objTemplate->{$section . 'ContainerClass'} = $objTemplate->layout->{'bootstrap_' . $section . '_width'};
-            }
-        } /**
-         * kept for BC with 4.13, custom elements and third-party bundles using legacy templates
-         */
-        else {
-            if ($objTemplate->layout_content_alignment) {
-                $alignClass = 'text-' . $objTemplate->layout_content_alignment;
+        $arrMain = [];
+        foreach((new $GLOBALS['responsive']['config']())->arrBreakpoints as $strBreakpoint => $arrBreakpoint){
+            $arrMain[$strBreakpoint] = 12 - ($arrBreakpoints[$strBreakpoint] % 12);
+        }
 
-                if (!in_array($alignClass, explode(' ', $objTemplate->class))) {
-                    $objTemplate->class .= ' ' . $alignClass;
-                }
-            }
+        $objTemplate->responsiveColsMain = $responsiveFrontendService->getColClasses(serialize($arrMain));
+
+
+        // Rows
+        $arrSections = ['Header', '', 'Footer'];
+        foreach ($arrSections as $section) {
+            $objTemplate->{"responsiveContainerSize$section"} = $objTemplate->layout->{"responsiveContainerSize$section"};
         }
     }
 }
