7a76aa24d45731e4c64de7fc2956ea93d7175019
falkgeist <falkvonfreigeist@gmail.com>
2026-06-18 18:17:40 +0200

    fix: add missing translations

---
+++ contao/languages/en/tl_layout.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_layout']['responsiveContainerSize'] = ["Container width"];
+$GLOBALS['TL_LANG']['tl_layout']['responsiveColsLeft'] = ["Left column width", "Width in columns"];
+$GLOBALS['TL_LANG']['tl_layout']['responsiveColsRight'] = ["Right column width", "Width in columns"];
